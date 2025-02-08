a204360a61ec127fbb93725e3b8bd2fb39d86cc5
Aftab Alam <aftabalamdlm@gmail.com>
2025-02-09 00:45:14 +0530

    fixed some minor issues and add an example

--- example/index.js
+++ example/index.js
@@ -1,6 +1,6 @@
-const { load } = require("nodejs-env");
+const { load } = require("nodejs-env-loader");
 
-const result = load();
+const result = load({ path: ".env" });
 console.log(result); // Check if .env file was loaded successfully
 console.log(process.env.URL); // Check loaded environment variables
 console.log(process.env.MY_KEY); // Check loaded environment variables
